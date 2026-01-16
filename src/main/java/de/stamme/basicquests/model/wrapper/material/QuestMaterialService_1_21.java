b8c0f50156f5e4cd7c4884a8696fc3107ce141c9
Anton Stamme <anton@stamme.de>
2026-01-16 21:20:48 +0100

    make pale oak logs progress chop log quests

--- src/main/java/de/stamme/basicquests/model/wrapper/material/QuestMaterialService_1_21.java
+++ src/main/java/de/stamme/basicquests/model/wrapper/material/QuestMaterialService_1_21.java
@@ -1,24 +1,37 @@
 package de.stamme.basicquests.model.wrapper.material;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
+
 import org.bukkit.Material;
 
-// 1.20 introduces Cherry Logs
 public class QuestMaterialService_1_21 extends QuestMaterialService {
+
+    private static final EnumMap<Material, EnumSet<Material>> ORE_VARIANTS = new EnumMap<>(Material.class);
+    private static final EnumSet<Material> LOG_MATERIALS = EnumSet.of(Material.ACACIA_LOG, Material.BIRCH_LOG, Material.DARK_OAK_LOG, Material.JUNGLE_LOG,
+            Material.OAK_LOG, Material.SPRUCE_LOG, Material.MANGROVE_LOG, Material.CHERRY_LOG, Material.PALE_OAK_LOG);
+
+    static {
+        ORE_VARIANTS.put(Material.LAPIS_ORE, EnumSet.of(Material.DEEPSLATE_LAPIS_ORE));
+        ORE_VARIANTS.put(Material.IRON_ORE, EnumSet.of(Material.DEEPSLATE_IRON_ORE));
+        ORE_VARIANTS.put(Material.COAL_ORE, EnumSet.of(Material.DEEPSLATE_COAL_ORE));
+        ORE_VARIANTS.put(Material.COPPER_ORE, EnumSet.of(Material.DEEPSLATE_COPPER_ORE));
+        ORE_VARIANTS.put(Material.DIAMOND_ORE, EnumSet.of(Material.DEEPSLATE_DIAMOND_ORE));
+        ORE_VARIANTS.put(Material.EMERALD_ORE, EnumSet.of(Material.DEEPSLATE_EMERALD_ORE));
+        ORE_VARIANTS.put(Material.GOLD_ORE, EnumSet.of(Material.DEEPSLATE_GOLD_ORE));
+        ORE_VARIANTS.put(Material.REDSTONE_ORE, EnumSet.of(Material.DEEPSLATE_REDSTONE_ORE));
+    }
+
     @Override
     public boolean isCorrectMaterialForQuest(Material questMaterial, Material blockMaterial) {
-        return (questMaterial == blockMaterial || (questMaterial == Material.LAPIS_ORE && blockMaterial == Material.DEEPSLATE_LAPIS_ORE)
-                || (questMaterial == Material.IRON_ORE && blockMaterial == Material.DEEPSLATE_IRON_ORE)
-                || (questMaterial == Material.COAL_ORE && blockMaterial == Material.DEEPSLATE_COAL_ORE)
-                || (questMaterial == Material.COPPER_ORE && blockMaterial == Material.DEEPSLATE_COPPER_ORE)
-                || (questMaterial == Material.DIAMOND_ORE && blockMaterial == Material.DEEPSLATE_DIAMOND_ORE)
-                || (questMaterial == Material.EMERALD_ORE && blockMaterial == Material.DEEPSLATE_EMERALD_ORE)
-                || (questMaterial == Material.GOLD_ORE && blockMaterial == Material.DEEPSLATE_GOLD_ORE)
-                || (questMaterial == Material.REDSTONE_ORE && blockMaterial == Material.DEEPSLATE_REDSTONE_ORE));
+        if (questMaterial == blockMaterial) {
+            return true;
+        }
+        return ORE_VARIANTS.getOrDefault(questMaterial, EnumSet.noneOf(Material.class)).contains(blockMaterial);
     }
 
     @Override
     public boolean isLogType(Material material) {
-        return (material == Material.ACACIA_LOG || material == Material.BIRCH_LOG || material == Material.DARK_OAK_LOG || material == Material.JUNGLE_LOG
-                || material == Material.OAK_LOG || material == Material.SPRUCE_LOG || material == Material.MANGROVE_LOG || material == Material.CHERRY_LOG);
+        return LOG_MATERIALS.contains(material);
     }
 }
